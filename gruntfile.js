5331851442e904b52350d4a0e8dbbea9197a6393
ziax <ziaxdk@gmail.com>
2014-06-21 10:42:18 +0200

    grunt

--- gruntfile.js
+++ gruntfile.js
@@ -3,13 +3,17 @@ module.exports = function(grunt) {
       mExpress = require('./lib/grunt/express.js')(grunt),
       mConcurrent = require('./lib/grunt/concurrent.js')(grunt),
       mWatch = require('./lib/grunt/watch.js')(grunt),
-      mWeinre = require('./lib/grunt/weinre.js')(grunt);
+      mWeinre = require('./lib/grunt/weinre.js')(grunt),
+      mCopy = require('./lib/grunt/copy.js')(grunt),
+      mClean = require('./lib/grunt/clean.js')(grunt);
 
 
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
 
+    clean: mClean,
+    copy: mCopy,
     weinre: mWeinre,
     concurrent: mConcurrent,
     express: mExpress,
@@ -18,4 +22,5 @@ module.exports = function(grunt) {
   
   grunt.registerTask('default', [ ]);
   grunt.registerTask('dev', [ 'express:dev', 'weinre:dev', 'watch' ]);
+  grunt.registerTask('build', [ 'clean:build', 'copy:build'  ]);
 };
