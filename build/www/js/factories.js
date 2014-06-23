044d273c1a486c7701ce8fb53e7e0611ddb7974c
ziax <ziaxdk@gmail.com>
2014-06-23 15:21:58 +0200

    wip

--- build/www/js/factories.js
+++
@@ -1,8 +0,0 @@
-// angular.module('ziaxgazapp.factories', [])
-
-// .factory('Hash', [function() {
-//   return function(uid, salt) {
-//     return CryptoJS.PBKDF2('1001', '4b88cb1e45d71528', { keySize: 32/4, iterations: 16 }).toString();
-//   };
-// }]);
-
