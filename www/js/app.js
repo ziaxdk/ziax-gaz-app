01de5b958009b0cfa0a4b92b1182d89c555c0820
ziax <ziaxdk@gmail.com>
2014-09-28 18:06:51 +0200

    res

--- www/js/app.js
+++ www/js/app.js
@@ -10,10 +10,6 @@ angular.module('ziaxgazapp', [
   function($ionicPlatform, $rootScope, $state, $timeout, User, Hardware, GPS, FINALS) {
 
   $ionicPlatform.ready(function() {
-    GPS.reset();
-    GPS.startGps();
-
-    $rootScope.user = User.get();
     console.log('ionicPlatform ready.');
     if(window.StatusBar) {
       StatusBar.styleDefault();
@@ -35,6 +31,10 @@ angular.module('ziaxgazapp', [
       console.log('backbutton');
     }, false);
   });
+  GPS.reset();
+  GPS.startGps();
+
+  $rootScope.user = User.get();
 
   // $rootScope.$on('$stateChangeError', function() {
   //   console.log('$stateChangeError', arguments);
